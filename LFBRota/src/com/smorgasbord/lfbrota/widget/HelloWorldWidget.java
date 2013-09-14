beab32c9f455d461afd8cb7ad7290bac3a6fa4af
steph <morgan_stephen@ymail.com>
2013-09-14 11:39:15 +0100

    Save Widget layout so far before breaking it all with a framelayout

--- LFBRota/src/com/smorgasbord/lfbrota/widget/HelloWorldWidget.java
+++ LFBRota/src/com/smorgasbord/lfbrota/widget/HelloWorldWidget.java
@@ -8,19 +8,25 @@ import com.smorgasbord.lfbrota.R;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 public class HelloWorldWidget extends AppWidgetProvider {
 
 	private SimpleDateFormat formatter = new SimpleDateFormat("EEEEEEEEE\nd MMM yyy");
 
+
+	public HelloWorldWidget() {
+		Log.d(":LFBRota","Hello World widget Constructor called");
+	}
+	
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		
 	String now = formatter.format(new Date());
 	
-		RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.lfbwidget_layout);
+		RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_hw_layout);
 		updateViews.setTextViewText(R.id.text, now);
 		appWidgetManager.updateAppWidget(appWidgetIds, updateViews);
 		
